e622baa37d70d07365144d0bcf457af185c13b53
Nick Moore <nick65@gmail.com>
2014-02-13 17:10:58 -0600

    Add livereload

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,23 @@
 var gulp = require('gulp')
   , gutil = require('gulp-util')
   , concat = require('gulp-concat')
-  , uglify = require('gulp-uglify');
+  , uglify = require('gulp-uglify')
+  , clean = require('gulp-clean')
+  , livereload = require('gulp-livereload')
+  , lr = require('tiny-lr')
+  , server = lr();
 
 var paths = {
-  scripts: ['bower_components/**/*.js', 'public/js/**/*.js']
+  scripts: ['bower_components/**/*.js', 'public/js/app.js', 'public/js/libs/pixi/bin/pixi.dev.js']
   ,styles: ['bower_components/**/*.js', 'public/css/**/*.css']
   ,assets: ['public/assets/*']
   ,main: ['public/index.html']
   ,server: ['./server.js']
 };
 
-gulp.task('default', ['scripts', 'styles', 'assets', 'main']);
+gulp.task('default', ['clean'], function() {
+  return gulp.start('scripts', 'styles', 'assets', 'main', 'server');
+});
 
 gulp.task('server', ['scripts', 'styles', 'assets', 'main'], function() {
   return gulp.src('./server.js')
@@ -20,20 +26,20 @@ gulp.task('server', ['scripts', 'styles', 'assets', 'main'], function() {
 
 gulp.task('main', function() {
   return gulp.src(paths.main)
-    .pipe(gulp.dest('dist/public'));
+    .pipe(gulp.dest('dist/public'))
+    .pipe(livereload(server));
 });
 
 gulp.task('scripts', function() {
   return gulp.src(paths.scripts)
-    .pipe(concat('main.js'))
-    .pipe(uglify('main.js'))
-    .pipe(gulp.dest('dist/public/js/main.js'));
+    .pipe(gulp.dest('dist/public/js'))
+    .pipe(livereload(server));
 });
 
 gulp.task('styles', function() {
   return gulp.src(paths.styles)
-    .pipe(concat('main.css'))
-    .pipe(gulp.dest('dist/public/css/main.css'));
+    .pipe(gulp.dest('dist/public/css'))
+    .pipe(livereload(server));
 });
 
 gulp.task('assets', function() {
@@ -47,6 +53,12 @@ gulp.task('clean', function() {
 });
 
 gulp.task('watch', function() {
-  gulp.watch('public/js/*.js', ['scripts']);
-  gulp.watch('public/css/*.css', ['styles']);
+  server.listen(35729, function(err) {
+    if (err) {
+      return console.log(err);
+    }
+    gulp.watch('public/js/*.js', ['scripts']);
+    gulp.watch('public/css/*.css', ['styles']);
+    gulp.watch('public/*.html', ['main']);
+  });
 });
